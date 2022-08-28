decf5f3935e662f952c707f9970474f0364f04e3
huangjia7 <zouyi202@163.com>
2022-08-28 10:05:27 +0800

    init

---
+++ August/src/main/java/cycle/Louzai1.java
@@ -0,0 +1,15 @@
+package cycle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Louzai1 {
+
+    @Autowired
+    private Louzai2 louzai2;
+
+    public void test1() {
+    }
+}
+
---
+++ August/src/main/java/cycle/Louzai2.java
@@ -0,0 +1,13 @@
+package cycle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Louzai2 {
+    @Autowired
+    private Louzai1 louzai1;
+
+    public void test2() {
+    }
+}
